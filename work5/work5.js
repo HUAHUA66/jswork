f536c5bf0b02f0b0e2ec2520a64dae3f1dee76f4
HUAHUA66 <55181278+HUAHUA66@users.noreply.github.com>
2019-10-15 13:19:08 +0800

    Add files via upload

--- work5/work5.js
+++ work5/work5.js
@@ -2,12 +2,12 @@
 
 let str = '<table border="1">'
 for(let a=1;a<10;++a){
-    str +='<tr>'
-    for(var b=1;b<=a;++b){
+ str +='<tr>'
+for(var b=1;b<=10;++b){
         //拼接单元格
-        str += '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>'
-        }
-        str +='</tr>'
+str += '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>'
+}
+str +='</tr>'
 }
 str +='<table>'
 //设置div的html文档内容
@@ -16,11 +16,22 @@ document.getElementById('table').innerHTML=str
 //倒九九乘法空表
 str = '<table border="1">'
 for(let i=9;i>0;--i){
-    str +='<tr>'
-    for(var j=i;j>0;--j){
-        str += '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>'
-    }
-    str +='</tr>'
+str +='<tr>'
+for(var j=i;j>0;--j){
+str += '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>'
+}
+str +='</tr>'
 }
 str += '</table>'
 document.getElementById('table2').innerHTML = str
+
+str = '<table border="1">'
+for(let c=9;c>0;--c){
+ str +='<tr>'
+for(var d=c;d>0;--d){
+str += '<td>' + d + '*' + c + '=' + (d * c) + '</td>'
+ }
+str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
