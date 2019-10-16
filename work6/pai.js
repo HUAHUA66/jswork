47bfcd7d60246f05c6cca6ba62855401ee51ab77
HUAHUA66 <2275797712@qq.com>
2019-10-16 15:45:35 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,32 @@
+function calculate(){
+    let num = document.getElementById('num').value 
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        //设置圆周率边输入框的值
+document.getElementById('num').value = pi(num)
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=1;i<10;++i){
+        sum +=</'sum'>
+
+    }
+    return sum 
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1;i<=num;++i){
+        sum +=</'sum'>
+    }
+    return sum 
+}
+function pi(sum){
+    let sum=0
+    for(let i=0;i<=num; ++i){
+        let divisor = oddFactorial(n)
+        sum
+    }
+    return sum 
+}
+calculate()
