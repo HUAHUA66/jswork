f464caaa0fda5746d01213c967d94cc7fa7ec698
HUAHUA66 <55181278+HUAHUA66@users.noreply.github.com>
2019-10-17 15:45:03 +0800

    Add files via upload

--- work6/pai.js
+++ work6/pai.js
@@ -8,25 +8,25 @@ document.getElementById('num').value = pi(num)
 }
 function factorial(n){
     let sum = 1
-    for(let i=1;i<10;++i){
-        sum +=</'sum'>
+    for(let i=1;i<=n;++i){
+        sum = sum+1 
 
     }
     return sum 
 }
 function oddFactorial(n){
     let sum = 1
-    for(let i=1;i<=num;++i){
-        sum +=</'sum'>
+    for(let i=1;i<=n;++i){
+        sum =2*n+1
     }
     return sum 
 }
-function pi(sum){
+function pi(num){
     let sum=0
     for(let i=0;i<=num; ++i){
         let divisor = oddFactorial(n)
-        sum
+        sum+=sum
     }
-    return sum 
+    return sum=
 }
 calculate()
