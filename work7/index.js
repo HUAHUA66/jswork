0aa99d4a6adbbe2d6c575bc12d05fc570fc285ac
HUAHUA66 <55181278+HUAHUA66@users.noreply.github.com>
2019-10-21 19:46:02 +0800

    Add files via upload

---
+++ work7/index.js
@@ -0,0 +1,15 @@
+function sort(method){
+    //读取待排序数据
+    let origin = document.getElementById('origin').value
+    let logs
+    console.log(method)
+    //根据传传入排序的方法调用不同的排序函数
+    if(method=='bubble'){
+        //调用bubblesort()冒泡排序函数获取排序结果
+        logs = bubbleSort(origin)
+    }else if (method=='insert'){
+        logs = insertSort(origin)
+    }
+    showDiv = document.getElementById('Anim') 
+    showDiv.innerHTML = logs
+}
