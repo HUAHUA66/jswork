8ae4d15e86a3e3a6c82c748c173a0757bfa33d9b
HUAHUA66 <55181278+HUAHUA66@users.noreply.github.com>
2019-12-03 20:52:24 +0800

    Add files via upload

---
+++ work12/index.js
@@ -0,0 +1,13 @@
+//获取当前年份
+let year = new Date().getFullYear()
+//调用calender函数，设置cal内容显示年份
+document.getElementById("cal").innerHTML = calendar(year)
+
+function setCalendar(year) {
+    //设置preyear
+    
+    //curyera
+    //设置netyear
+    //设置cal显示年份
+
+}
